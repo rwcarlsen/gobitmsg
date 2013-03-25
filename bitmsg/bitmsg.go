bd78b25be64c08a1c1d3f6890b1a77c084efc766
Robert Carlsen <rwcarlsen@gmail.com>
2013-03-25 14:34:21 -0500

    started to sketch out types

--- bitmsg/bitmsg.go
+++ bitmsg/bitmsg.go
@@ -5,4 +5,26 @@ const (
 	Version = "0.2.7"
 )
 
+type NodeSet struct {
+	Nodes []*Node
+}
+
+func (s *NodeSet) Add(nodes ...*Node) {
+	s.Nodes = append(s.Nodes, nodes...)
+}
+
+func (s *NodeSet) InStream(stream int) []*Node {
+	nodes := make([]*Node, 0)
+	for _, n := range s.Nodes {
+		if n.Stream == stream {
+			nodes = append(nodes, n)
+		}
+	}
+	return nodes
+}
+
+type Node struct {
+	Stream int
+	Friends *NodeSet
+}
 
