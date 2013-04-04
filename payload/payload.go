93f2f6c97047590a2a28b3822fe49844e590c616
Robert Carlsen <rwcarlsen@gmail.com>
2013-04-04 17:11:15 -0500

    implemented proof of work

--- payload/payload.go
+++ payload/payload.go
@@ -8,15 +8,41 @@ import (
 	"github.com/rwcarlsen/gobitmsg/message"
 )
 
+const (
+	PowExtraLen      = 14000
+	PowTrialsPerByte = 320
+)
+
 var order = message.Order
 
 // RandNonce is used to detect connections to self
 var RandNonce = uint64(rand.Uint32())
 
-// proofOfWork returns a verifiable proof-of-work hash for data.
-func proofOfWork(data []byte) []byte {
-	fuzz := (rand.Float64() - 0.5) * 300
-	t := time.Now().Unix() + fuzz
+func fuzzTime() time.Time {
+	t := time.Now()
+	fuzz := time.Duration((rand.Float64()-0.5)*300) * time.Second
+	return t.Add(fuzz)
+}
+
+// proofOfWork returns a pow nonce for data.
+func proofOfWork(data []byte) (nonce uint64, err error) {
+	h := sha512.New()
+	h.Write(data)
+	kernel := h.Sum(nil)
+
+	trial := math.MaxUint64
+	target := math.MaxUint64 / ((len(data) + PowExtraLen + 8) * PowTrialsPerByte)
+	for nonce = 0; trial > target; nonce++ {
+		h.Reset()
+		h.Write(append(packUint(order, nonce), kernel...))
+		h.Write(h.Sum(nil))
+		sum := h.Sum(nil)
+		trial = order.Uint64(sum[len(sum)-8 : len(sum)])
+		if nonce == math.MaxUint64 {
+			return 0, errors.New("payload: Failed to calculate POW")
+		}
+	}
+	return nonce, nil
 }
 
 type Version struct {
