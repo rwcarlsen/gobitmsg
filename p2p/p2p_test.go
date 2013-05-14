045773c4312ea2ad3b5ba68ed9ad9639770ad2d1
Robert Carlsen <rwcarlsen@gmail.com>
2013-05-14 14:30:34 -0500

    fixed p2p send/receive test

--- p2p/p2p_test.go
+++ p2p/p2p_test.go
@@ -8,28 +8,34 @@ import (
 	"github.com/rwcarlsen/gobitmsg/msg"
 )
 
-type TestHandler struct {}
+type SendHandler struct {
+	Resp *msg.Msg
+}
 
-func (h *TestHandler) Handle(w io.Writer, m *msg.Msg) {
+func (h *SendHandler) Handle(w io.WriteCloser, m *msg.Msg) {
+	h.Resp = m
+	w.Close()
+}
+
+type RecvHandler struct {}
+
+func (h *RecvHandler) Handle(w io.WriteCloser, m *msg.Msg) {
 	w.Write(m.Encode())
 }
 
 func TestSendAndRespond(t *testing.T) {
-	node1 := NewNode("127.0.0.1:22334", &TestHandler{})
-	if err := node1.Start(); err != nil {
-		t.Fatalf("node1 failed to start: %v", err)
+	node := NewNode("127.0.0.1:22334", &RecvHandler{})
+	if err := node.Start(); err != nil {
+		t.Fatalf("node failed to start: %v", err)
 	}
 
-	node2 := NewNode("127.0.0.1:22335", &TestHandler{})
-	if err := node2.Start(); err != nil {
-		t.Fatalf("node2 failed to start: %v", err)
-	}
+	p := NewPeer("127.0.0.1:22334")
 
 	m := msg.New(msg.Cversion, []byte("hello from node1"))
-	resp, err := Send(node2.Addr(), m)
-	if  err != nil {
-		t.Errorf("node1 -> node2 send failed: %v", err)
-	} else if !bytes.Equal(m.Payload(), resp.Payload()) {
-		t.Errorf("node2 -> node1 response failed: expected %s, got %s", m.Payload(), resp.Payload())
+	h := &SendHandler{}
+	if err := p.Send(m, h); err != nil {
+		t.Errorf("send to peer node failed:", err)
+	} else if !bytes.Equal(m.Payload(), h.Resp.Payload()) {
+		t.Errorf("echo response failed: expected %s, got %s", m.Payload(), h.Resp.Payload())
 	}
 }
