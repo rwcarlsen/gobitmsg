bb24accbcf18cbd854fd53c21d384638c9dc31b6
Robert Carlsen <rwcarlsen@gmail.com>
2013-05-16 13:50:04 -0500

    tmp - version encode/decode bug...

--- p2p/p2p_test.go
+++ p2p/p2p_test.go
@@ -1,11 +1,14 @@
 package p2p
 
 import (
-	"bytes"
 	"net"
 	"testing"
+	"log"
+	"os"
+	"time"
 
 	"github.com/rwcarlsen/gobitmsg/msg"
+	"github.com/rwcarlsen/gobitmsg/payload"
 )
 
 type RecvHandler struct{}
@@ -18,26 +21,50 @@ func (h *RecvHandler) Handle(conn net.Conn) {
 	conn.Write(m.Encode())
 }
 
-func TestSendAndRespond(t *testing.T) {
-	node := NewNode("127.0.0.1:22334", &RecvHandler{})
-	if err := node.Start(); err != nil {
-		t.Fatalf("node failed to start: %v", err)
+func newTestNode(name string, ip string, port int) *Node {
+	addr := &payload.AddressInfo{
+		Time:     time.Now(),
+		Stream:   1,
+		Services: 1,
+		Ip:       ip,
+		Port:     port,
+	}
+	ver := &payload.Version{
+		Services:  1,
+		Timestamp: time.Now(),
+		ToAddr:    addr,
+		FromAddr:  addr,
+		UserAgent: "Go bitmessage Daemon",
+		Streams:   []int{1},
+	}
+	return &Node{
+		Addr: addr.Addr(),
+		Log: log.New(os.Stdout, name + ": ", log.LstdFlags),
+		Objects: make(chan *msg.Msg),
+		Ver: make(chan *VerResp),
+		MyVer: ver,
+		MyAddrList: []*payload.AddressInfo{},
+		MyInvList: [][]byte{},
+	}
+}
+
+func TestVersionExchange(t *testing.T) {
+	node1 := newTestNode("node1", "127.0.0.1", 22334)
+	if err := node1.Start(); err != nil {
+		t.Fatalf("node1 failed to start: %v", err)
 	}
 
-	m := msg.New(msg.Cversion, []byte("hello from node1"))
-	conn, err := Dial("127.0.0.1:22334")
+	node2 := newTestNode("node2", "127.0.0.1", 22335)
+	if err := node2.Start(); err != nil {
+		t.Fatalf("node2 failed to start: %v", err)
+	}
+
+	go func() {
+		<- node1.Ver
+	}()
+
+	err = node1.VersionExchange(node2.MyVer.FromAddr)
 	if err != nil {
-		t.Errorf("connection to peer node failed:", err)
-	}
-	defer conn.Close()
-
-	conn.Write(m.Encode())
-	resp, err := msg.Decode(conn)
-	if err != nil {
-		t.Errorf("response from peer node failed:", err)
-	}
-
-	if !bytes.Equal(m.Payload(), resp.Payload()) {
-		t.Errorf("echo response failed: expected %s, got %s", m.Payload(), resp.Payload())
+		t.Fatal(err)
 	}
 }
